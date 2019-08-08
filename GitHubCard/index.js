ea2a8ea65c5cc29affa52b41a5734e00ed559336
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-08 14:20:19 -0700

    Refactor

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,47 +3,34 @@
            https://api.github.com/users/<your name>
 */
 
-function axiosFunction(username){
+function axiosFunction(username) {
   axios
-  .get(`https://api.github.com/users/${username}`)
-  .then(response => {
-    //response is my object
-    // console.log(response);
-    cardGenerator(response.data);
-    // resolve(response);
-    return username;
-  })
-  .then(username =>{
-    axios.get(`https://api.github.com/users/${username}/followers`)
-    .then(response=>{
-      // console.log(response);
-      let friendList = response.data;
-
-
-
-      friendList.forEach((item)=>{
-
-        console.log(item.login);
-
-                axios
-          .get(`https://api.github.com/users/${item.login}`)
-          .then(response => {
-            
-            cardGenerator(response.data);
-            
-          })
-
-      });
+    .get(`https://api.github.com/users/${username}`)
+    .then(response => {
+      cardGenerator(response.data);
+      return username;
     })
-    .catch(error=>{
+    .then(username => {
+      axios
+        .get(`https://api.github.com/users/${username}/followers`)
+        .then(response => {
+          let friendList = response.data;
+          friendList.forEach(item => {
+            axios
+              .get(`https://api.github.com/users/${item.login}`)
+              .then(response => {
+                cardGenerator(response.data);
+              });
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    })
+    .catch(error => {
       console.log(error);
     });
-  })
-  .catch(error => {
-    console.log(error);
-  });
 }
-
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -74,7 +61,6 @@ const followersArray = [
   "Cerberean"
 ];
 // This array is not used because of the stretch goal
-
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -137,7 +123,7 @@ function cardGenerator(obj) {
   let bioP = document.createElement("p");
   bioP.textContent = `Bio: ${obj.bio}`;
 
-  let chartImg = document.createElement('img');
+  let chartImg = document.createElement("img");
   chartImg.src = `http://ghchart.rshah.org/${obj.login}`;
   chartImg.style.width = "100%";
 
@@ -166,5 +152,5 @@ function cardGenerator(obj) {
 axiosFunction("krishan-nattar");
 
 // followersArray.forEach((item, index, array)=>{
-//   axiosFunction(item);  
+//   axiosFunction(item);
 // });
